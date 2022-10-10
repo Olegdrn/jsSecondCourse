694bbf6ad96c1104114aa15f31dad94ee14e36ba
Olegdrn <105755089+Olegdrn@users.noreply.github.com>
2022-10-10 17:39:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    userSearch: '',
+    showCart: false,
+    cartItems: [],
+    filtered: [],
+    products: [],
+  },
+  methods: {
+    getJson(url) {
+      return fetch(url)
+        .then(result => result.json())
+        .catch(error => console.log(error))
+    },
+    addProduct(item) {
+      this.getJson(`${API}/addToBasket.json`)
+        .then(data => {
+          if (data.result === 1) {
+            let find = this.cartItems.find(el => el.id_product === item.id_product);
+            if (find) {
+              find.quantity++;
+            } else {
+              const prod = Object.assign({ quantity: 1 }, item);
+              this.cartItems.push(prod)
+            }
+          }
+        })
+    },
+    remove(item) {
+      this.getJson(`${API}/deleteFromBasket.json`)
+        .then(data => {
+          if (data.result === 1) {
+            if (item.quantity > 1) {
+              item.quantity--;
+            } else {
+              this.cartItems.splice(this.cartItems.indexOf(item), 1);
+            }
+          }
+        })
+    },
+    filter(userSearch) {
+      let regexp = new RegExp(this.userSearch, 'i');
+      this.filtered = this.products.filter(el => regexp.test(el.product_name));
+    }
+
+  },
+  mounted() {
+    this.getJson(`https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json`)
+      .then(data => {
+        for (let item of data.contents) {
+          this.cartItems.push(item);
+        }
+      });
+    this.getJson(`https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json`)
+      .then(data => {
+        for (let item of data) {
+          this.$data.products.push(item);
+          this.$data.filtered.push(item);
+        }
+      });
+  }
+
+});
+
